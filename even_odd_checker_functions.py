0bfedef5aacae966b77aae52e19a87c66c411531
cristinescheiquilay <scheiyaliuq@gmail.com>
2025-03-16 16:47:06 +0800

    Submitting Python Week 05 - Laboratory # 20

---
+++ even_odd_checker_functions.py
@@ -0,0 +1,17 @@
+def get_integer_input() -> int:
+    while True:
+        try:
+            return int(input("Enter an integer: "))
+        except ValueError:
+            print("Error: Please enter a valid integer.")
+
+def check_even_odd(number: int) -> str:
+    return f"{number} is an Even number." if number % 2 == 0 else f"{number} is an Odd number."
+
+def main():
+    number = get_integer_input()
+    result = check_even_odd(number)
+    print(result)
+
+if __name__ == "__main__":
+    main()
